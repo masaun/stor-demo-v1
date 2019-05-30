38d0b8b91183166891118d4d7f4f75cea58d34b8
masaun <masanoriuno@gmail.com>
2019-05-30 11:29:32 +0200

    Import Ownable.sol and SafeMath.sol from openzeppelin-solidity-v2.1.1

--- contracts/List.sol
+++ contracts/List.sol
@@ -1,6 +1,10 @@
 pragma solidity ^0.5.0;
 
+import "openzeppelin-solidity-2.1.1/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity-2.1.1/contracts/ownership/Ownable.sol";
+
+
 contract List {
 	// in progress
-    
+
 }
