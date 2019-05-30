8fb577ff11a87e47715fac07b60d4a17e0e89834
masaun <masanoriuno@gmail.com>
2019-05-30 11:55:48 +0200

    Update List.sol

--- contracts/List.sol
+++ contracts/List.sol
@@ -4,7 +4,30 @@ import "openzeppelin-solidity-2.1.1/contracts/math/SafeMath.sol";
 import "openzeppelin-solidity-2.1.1/contracts/ownership/Ownable.sol";
 
 
-contract List {
-	// in progress
+contract List is SafeMath, Ownable {
+
+    struct Production {
+        address addr;
+        string town;
+    }
+    Production[] public productions; 
+
+
+    constructor () {
+        // Put something
+    }
+
+
+    function Register(address _addr, string _town) public returns (address, string memory) {
+        
+        Production memory production;
+        production._addr;
+        production._town;
+        
+        productions.push(production);
+
+        return (_addr, _town);
+    }
+    
 
 }
