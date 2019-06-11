e126d846238432c3ce5408711bd4b2c943ec3143
masaun <masanoriuno@gmail.com>
2019-06-11 10:19:08 +0200

    Fix style of Pie-chart by using react-minimal-pie-chart

--- client/src/App.js
+++ client/src/App.js
@@ -789,6 +789,9 @@ class App extends Component {
                 { title: 'Two', value: 15, color: '#C13C37' },
                 { title: 'Three', value: 20, color: '#6A2135' },
               ]}
+              lineWidth={15}
+              paddingAngle={5}
+              lengthAngle={-360}
             />;
 
           </div>
