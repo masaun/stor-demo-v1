0c84b02ac7b70210cf8040972b705653a2ef5c7b
masaun <masanoriuno@gmail.com>
2019-06-13 09:06:11 +0200

    Add StakingToken.sol for using stake of predicition market

--- contracts_in_progress/Gamification.sol
+++ contracts_in_progress/Gamification.sol
@@ -1,5 +1,8 @@
 pragma solidity ^0.5.0;
 
+import "openzeppelin-solidity-2.1.1/contracts/token/ERC20/IERC20.sol";
+import '@daostack/infra/contracts/votingMachines/AbsoluteVote.sol';
+
 
 /**
  * The Gamification contract that provide prediction market of topic of energy
