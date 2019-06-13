5416b848d54211ee48bf8a12b899ff4f5b795b3b
masaun <masanoriuno@gmail.com>
2019-06-13 09:54:26 +0200

    Add struct of PredictionContent and function of transferStakingToken

--- contracts_in_progress/Gamification.sol
+++ contracts_in_progress/Gamification.sol
@@ -9,17 +9,34 @@ import '@daostack/infra/contracts/votingMachines/AbsoluteVote.sol';
  */
 contract Gamification {
 
-    struct Something {
-        // in progress
-        address addr;
-        uint amount;
+    struct PredictionContent {
+        bytes32 topic;           // Prediction Topic title
+        bytes32 detail;          // Prediction Topic detail
+        string memory category;  // Prediction Topic category
     }
     
-
 
     constructor () public {
         // in progress
     }
+
+
+
+
+    /* @dev Transfer staking token when Predicter join some prediction of topic */
+    function transferStakingToken(StakingToken _stakingToken, uint256 _amount, address _Predicter) public returns (bool) {
+        // in progress  
+    }
+    
+    function buyStakingToken(uint256 _amount, address _Predicter) public returns (bool) {
+        // in progress
+    }
+
+    function sellStakingToken(uint256 _amount, address _Predicter) public returns (bool) {
+        // in progress
+    }
+    
+
 
 }
 
