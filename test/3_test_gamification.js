f0e6323a24c5b22b2ba9989ea631f9a86cb64ad3
masaun <masanoriuno@gmail.com>
2019-06-14 09:31:15 +0200

    Get contract address of StakingToken.sol for assigning to arg of transferStakingToken function

--- test/3_test_gamification.js
+++ test/3_test_gamification.js
@@ -1,11 +1,17 @@
 const Gamification = artifacts.require('Gamification.sol')
+const StakingToken = artifacts.require("StakingToken.sol");  // For getting contract address of StakingToken.sol
+
 
 contract('Asset', (accounts) => {
     it('Execute transferStakingToken function', async () => {
         const accounts = await web3.eth.getAccounts();
 
+        // Get contract address of StakingToken.sol
+        const _stakingToken = StakingToken.address;
+        console.log('=== contract address of StakingToken.sol ===', _stakingToken);
+
         // Value of argument for test
-        const _stakingToken = "0x3d95c3daa594bf5e21a701402e82d7f7b846da18";  // Contract address of StakingToken.sol (which mean is address(this))
+        //const _stakingToken = "0x3d95c3daa594bf5e21a701402e82d7f7b846da18";  // Contract address of StakingToken.sol (which mean is address(this))
         const _predictionContentId = 1;
         const _participant = "0x81d67c9a6b6b76c684f18a45e65af61efc52c16b";
         const _amount = 10000;
@@ -15,7 +21,7 @@ contract('Asset', (accounts) => {
         const response = await contract.methods.transferStakingToken(_stakingToken, _predictionContentId, _participant, _amount).send({ from: accounts[0], gas: 3000000 });
 
         // Debug
-        console.log('=== response of transferStakingToken function ===', response);  // Result: 
+        console.log('=== response of transferStakingToken function ===', response);
 
     })
 })
