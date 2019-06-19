1a4c5b44b60c58c5921d9a4d0e63bb3982068a2e
masaun <masanoriuno@gmail.com>
2019-06-19 12:03:55 +0200

    Fix layout of Cards side by side by using style of display: inline-flex

--- client/src/components/Map/GoogleMapReact.js
+++ client/src/components/Map/GoogleMapReact.js
@@ -21,7 +21,7 @@ class SimpleMap extends Component {
   render() {
     return (
       // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
+      <div style={{ height: '60vh', width: '100%' }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'BLskCNDcpuIKPZGUlFGHcvMKYYtRJNBADWhUSaL' }}  // Using Sample-Key (If you use it, ypu have to replace Sample-Key with your API-KEY)
           //bootstrapURLKeys={{ key: GoogleMapReact_API_KEY }}
